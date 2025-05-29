ec7934eb2ce9fe573403549a7f19e41aa5fac4d4
ebrahim <ebrahimasil@gmail.com>
2025-05-29 19:56:15 +0430

    chore: finalize project with complete features, styling, and documentation

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import TaskList from "./components/TaskList";
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-50 p-6 text-black/70">
-      <h1 className="text-3xl font-bold text-center mb-6">
+      <h1 className="text-3xl font-bold text-center mb-6 mt-4">
         Redux Task Manager
       </h1>
       <div className="max-w-screen-xl mx-auto flex items-center justify-center">
